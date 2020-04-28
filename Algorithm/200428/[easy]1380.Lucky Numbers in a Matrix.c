f41e3a5d705ccc5fa370e0d34bb46f9101aa430a
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-04-28 23:29:09 +0900

    Create [easy]1380.Lucky Numbers in a Matrix.c

---
+++ Algorithm/200428/[easy]1380.Lucky Numbers in a Matrix.c
@@ -0,0 +1,10 @@
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* luckyNumbers (int** matrix, int matrixSize, int* matrixColSize, int* returnSize){
+    printf("matrixSize: %d\n", matrixSize);
+    printf("matrixColSize: %d\n", matrixColSize[2]);
+    
+    return NULL;
+}
