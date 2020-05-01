e6acafc878b7873e0b0785e860ab6d024f9b9526
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-05-02 05:03:39 +0900

    Update chardev_usertest.c

--- work/chardevice/chardev_usertest.c
+++ work/chardevice/chardev_usertest.c
@@ -11,13 +11,16 @@ int main(void)
 	static char buff[256];
 	int fd;
 
+	// open chardev0, created and registered by chardev module
 	if ((fd = open("/dev/chardev0", O_RDWR)) < 0) {
 		printf("Failed to open /dev/chardev0: abort\n");
 	}
+	// read from chardev0: data originally stored in kernel buffer
 	if (read(fd, buff, TEXT_LEN) < 0) {
 		printf("Failed to read\n");
 	} else printf("%s\n", buff);
-
+	
+	// write new data to kernel
 	if (write(fd, "AAAAAAAAAAA", TEXT_LEN) < 0) {
 		printf("Failed to write string\n");
 	}
