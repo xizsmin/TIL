e4fdf8fdb46d6e788d53d5e299f60a97f58983f2
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-05-13 23:08:32 +0900

    Update 1387. Sort Integers by The Power Value.c

--- Algorithm/200511/1387. Sort Integers by The Power Value.c
+++ Algorithm/200511/1387. Sort Integers by The Power Value.c
@@ -1,30 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct pow {
+	int num;
+	int power;
+};
 
 int check_power_of_two(unsigned int x)
 {
 	return (x && ((x & (x - 1)) ==0));
 }
 
-int kth(unsigned int x, int lev)
+int kth(struct pow x, int lev)
 {
-	if (x == 1) return lev;
-	if (check_power_of_two(x)) {
+	if (x.power == 1) return lev;
+	if (check_power_of_two(x.power)) {
 		int l=0;
-		while( x >>= 1) ++l;
+		while( x.power >>= 1) ++l;
 		return lev+l;
 	}
-	if (!(x & 1)) return kth(x/2, lev+1);
-	else return kth(3*x+1, lev+1);
+
+	if (!(x.power & 1)) {
+		x.power >>= 1;
+		return kth(x, lev+1);
+	}
+	else {
+		x.power = x.power * 3 + 1;
+		return kth(x, lev+1);
+	}
 
 }
 
+int static compare (const void* first, const void* second)
+{
+	struct pow *_first = (struct pow *)first;
+	struct pow *_second = (struct pow *)second;
+
+	if (_first->power > _second->power)
+        return 1;
+ 
+    else if (_first->power < _second->power)
+        return -1;
+    else
+        return 0;
+}
+
+int getKth(int lo, int hi, int k)
+{
+	struct pow *tmp = (struct pow *)malloc(sizeof(struct pow) * (hi-lo+1) );
+	for(int i=lo; i<=hi; ++i) {
+		tmp[i-lo].num = i;
+		tmp[i-lo].power = i;
+		tmp[i-lo].power = kth(tmp[i-lo], 0);
+	}
+	qsort(tmp, hi-lo+1, sizeof(struct pow), compare);
+	return tmp[k-1].num;
+}
 
 int main(int argc, char **argv)
 {
-
-	for(int i=1; i<1000; ++i) \
-		printf("[%d]%d\n", i, kth((unsigned int)i, 0));
-
+    printf("%d\n", getKth(7,11,4));
 	return 0;
 }
 
