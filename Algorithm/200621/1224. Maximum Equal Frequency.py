75a8095894d7f15dca8ec7866737624165b5ceaf
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-06-26 23:51:14 +0900

    Update 1224. Maximum Equal Frequency.py

--- Algorithm/200621/1224. Maximum Equal Frequency.py
+++ Algorithm/200621/1224. Maximum Equal Frequency.py
@@ -25,3 +25,30 @@ for i in range(len_nums):
 	
 	
 	
+'''
+cnt_nums = Counter(nums)
+max_cnt = max(cnt_nums.values())
+min_cnt = min(cnt_nums.values())
+print('max_cnt:',max_cnt,'min_cnt:',min_cnt)
+
+
+i=0
+for n in reversed(nums):
+	print('removing', n)
+	cnt_nums[n] = cnt_nums[n]-1
+	print('  cnt_nums[',n,']:', cnt_nums[n]+1,'---->', cnt_nums[n])
+
+	print('max_cnt:',max_cnt,'min_cnt:',min_cnt)
+	if max_cnt == min_cnt+1:
+		print(Counter(cnt_nums.values()))
+		if (Counter(cnt_nums.values())[max_cnt] == 1):
+			print('****', len(nums)-i-1)
+			break
+	if cnt_nums[n] == 0:
+		cnt_nums.pop(n)
+	elif cnt_nums[n] < min_cnt:
+		min_cnt = cnt_nums[n]
+
+
+	i = i+1
+'''
