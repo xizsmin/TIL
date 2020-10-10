a6fb93b4bfc8e7d514f05ba03b7d0e5ae6a9dc7f
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-10-10 23:01:25 +0900

    Update 1608. Special Array With X Elements Greater Than or Equal X.py

--- Algorithm/201007/1608. Special Array With X Elements Greater Than or Equal X.py
+++ Algorithm/201007/1608. Special Array With X Elements Greater Than or Equal X.py
@@ -6,6 +6,24 @@ Notice that x does not have to be an element in nums.
 Return x if the array is special, otherwise, return -1. It can be proven that if nums is special, the value for x is unique.
 
 '''
+'''
+Algorithm:
+1. Let's say desc_nums[] = nums sorted in descending order.
+2. Generalization:
+   - To say i numbers in nums are greater than or equal to i:
+   - we need to see if desc_nums[i-1] >= i. 
+   - Now, to ensure that there are EXACTLY i numbers meet the condition,
+   - desc_nums[i] must be smaller than i.
+e.g.,
+   0 1 2 3 4 5
+  -------------    => desc_nums[2] > 3 and desc_nums[3] < 3: There are exactly 3 numbers in nums greater than 3
+   9 6 4 1 0 0
+   
+   0 1 2 3 4 5
+  -------------    => desc_nums[2] > 3 and desc_nums[3] > 3: 3 can't be answer (No answer exists in this case).
+   9 6 4 3 0 0
+   
+'''
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         
@@ -19,3 +37,5 @@ class Solution:
                     ans = i
                 break
         return ans
+
+    
