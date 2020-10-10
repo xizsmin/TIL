5168005448068ae56bf215dd294ab8255a2c42a0
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-10-10 23:17:01 +0900

    Update 1529. Bulb Switcher IV.py

--- Algorithm/201007/1529. Bulb Switcher IV.py
+++ Algorithm/201007/1529. Bulb Switcher IV.py
@@ -1,6 +1,23 @@
 # https://leetcode.com/problems/bulb-switcher-iv/submissions/
+'''
+There is a room with n bulbs, numbered from 0 to n-1, arranged in a row from left to right. Initially all the bulbs are turned off.
 
+Your task is to obtain the configuration represented by target where target[i] is '1' if the i-th bulb is turned on and is '0' if it is turned off.
 
+You have a switch to flip the state of the bulb, a flip operation is defined as follows:
+
+Choose any bulb (index i) of your current configuration.
+Flip each bulb from index i to n-1.
+When any bulb is flipped it means that if it is 0 it changes to 1 and if it is 1 it changes to 0.
+
+Return the minimum number of flips required to form target.
+'''
+
+'''
+TRICK:
+   - We need to flip the bulb everytime we encounter one in a different state:
+   - i.e., we must check every bulb in different state with the previous one.
+'''
 class Solution:
     def minFlips(self, target: str) -> int:
         cnt = 0
