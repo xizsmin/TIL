fedb129e61afc9d16efa568680b8634a32d55edc
xizsmin <57291261+xizsmin@users.noreply.github.com>
2020-12-14 23:24:11 +0900

    Create 116.Populating Next Right Pointers in Each Node.py

---
+++ Algorithm/Excercise/116.Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,51 @@
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+'''
+You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+
+Example 1:
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+'''
+
+'''
+TRICK:
+
+My left's next == My right
+My right's next == My next's left (if exists)
+
+'''
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root == None:
+            return root
+        if root.left:
+            root.left.next = root.right
+        if root.right:
+            if root.next:
+                root.right.next = root.next.left
+        self.connect(root.left)
+        self.connect(root.right)
+        return root
+        
+        
+        
