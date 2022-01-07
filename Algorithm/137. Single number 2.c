244029b1612bfc27735e2979f6178fd454d090c0
xizsmin <57291261+xizsmin@users.noreply.github.com>
2022-01-07 18:53:28 +0900

    Update and rename 137. Single number 2.py to 137. Single number 2.c

--- Algorithm/137. Single number 2.c
+++ Algorithm/137. Single number 2.c
@@ -1,5 +1,4 @@
-
-"""
+/*
 Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -14,7 +13,17 @@ Example 2:
 
 Input: nums = [0,1,0,1,0,1,99] Output: 99 
 
- 
 
 
-"""
+*/
+
+int singleNumber(int* nums, int numsSize){
+    int one = 0, two = 0;
+    for (int i = 0; i < numsSize; i++) {
+        int one_ = (one ^ nums[i]) & ~two;
+        int two_ = nums[i] & one | ~nums[i] & two;
+        one = one_;
+        two = two_;
+    }
+    return one;
+}
