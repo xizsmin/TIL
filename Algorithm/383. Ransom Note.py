b109144a84cb78900652104c3ee3b65e99d09b84
xizsmin <57291261+xizsmin@users.noreply.github.com>
2022-01-13 21:27:24 +0900

    Create 383. Ransom Note.py

---
+++ Algorithm/383. Ransom Note.py
@@ -0,0 +1,38 @@
+"""
+Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+"""
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        rDict = dict.fromkeys(string.ascii_lowercase, 0)
+        mDict = dict.fromkeys(string.ascii_lowercase, 0)
+        
+        #print(rDict)
+        for r in ransomNote:
+            rDict[r] = rDict[r]+1
+            
+        for m in magazine:
+            mDict[m] = mDict[m]+1
+            
+        for k in mDict.keys():
+            if rDict[k] > mDict[k]:
+                return False
+        return True
